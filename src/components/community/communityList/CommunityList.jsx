2e210f301abfd3efafebe4f132f3927ec0c4ad0e
officefish <futuresimple@yandex.ru>
2021-08-25 15:47:24 +0300

    move ajax request from constructor to componentDidMount

--- src/components/community/communityList/CommunityList.jsx
+++ src/components/community/communityList/CommunityList.jsx
@@ -2,43 +2,17 @@ import Member from "./Member";
 import * as axios from 'axios'
 import React from "react";
 
-/*
-let CommulityList = (props) =>
-{
-
-    let update = props.update
-
-    if (props.data.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-        .then(response => {
-            update(response.data.items)
-        })
-    }
-
-    
-    let members = props.data
-        .map( user => 
-        <Member 
-            data={user} 
-            follow={props.follow} 
-            unfollow={props.unfollow}
-            update={props.update}
-            key={user.id}
-         /> )
-    return <div>{members}</div>
-}
-export default CommulityList;
-*/
-
 class CommunityList extends React.Component {
     constructor (props) {
         super(props)
 
-        /** External callback when getting response if dynamicly request for current list values */
+        /** External callback of GET:users, when getting response from remote server */
         this.update = props.update
+    }
 
-        /** if list of community is empty in constructing stage, request get them from remore server */
-        if (props.data.length === 0) {
+    componentDidMount () {
+         /** If list of community members is empty in constructing stage, request them from remore server */
+         if (this.props.data.length === 0) {
             axios
                 .get('https://social-network.samuraijs.com/api/1.0/users')
                 .then(response => {
