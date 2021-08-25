63110d0bf75cfdf9112ab45daea3a557635a2753
officefish <futuresimple@yandex.ru>
2021-08-25 15:08:39 +0300

    refactor CommunityList as class component

--- src/components/community/communityList/CommunityList.jsx
+++ src/components/community/communityList/CommunityList.jsx
@@ -1,6 +1,8 @@
 import Member from "./Member";
 import * as axios from 'axios'
+import React from "react";
 
+/*
 let CommulityList = (props) =>
 {
 
@@ -26,3 +28,36 @@ let CommulityList = (props) =>
     return <div>{members}</div>
 }
 export default CommulityList;
+*/
+
+class CommunityList extends React.Component {
+    constructor (props) {
+        super(props)
+
+        /** External callback when getting response if dynamicly request for current list values */
+        this.update = props.update
+
+        /** if list of community is empty in constructing stage, request get them from remore server */
+        if (props.data.length === 0) {
+            axios
+                .get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    this.update(response.data.items)
+                })
+        }
+    }
+
+    render () {
+        let members = this.props.data
+        .map( user => 
+        <Member 
+            data={user} 
+            follow={this.props.follow} 
+            unfollow={this.props.unfollow}
+            update={this.props.update}
+            key={user.id}
+         /> )
+        return <div>{members}</div>
+    } 
+}
+export default CommunityList
